09282b8ea4828edc6ea5bd36614c30108584fcf9
NataliyaPavlova <nataliya.s.pavlova@gmail.com>
2018-08-07 13:15:11 +0300

    new silence words and backtick

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 not_words = ['not', "n't", 'stop', 'cease', 'wind down', 'off', 'halt', 'shut down',
-             'dont', "don't", 'can\'t', 'mustn\'t', 'couldn\'t', 'doesn\'t']
+             'dont', "don'' ' ''t", 'can\'t', 'mustn\'t', 'couldn\'t', 'doesn\'t']
 wise_words = ['wise', 'wisdom', 'smart', 'clever', 'intelligent', 'sophisticated', 'sensible']
 curse_words = ['angry', 'fuck', 'wtf', 'furious', 'evil',
                'grumpy', 'yelling', 'kill', 'shit', 'damn',
                'asshole', 'ftw', 'stupid', 'bitch',
                'whore', 'crap', 'bastard', 'jerk', 'slut', 'faggot']
-silence_words = ['Who knows - does not say. Who says - does not know',
-                 'Speech is silver, silence is golden',
-                 'A shut mouth catches no flies']
+silence_words = ["What shall we do with a drunken sailor? \nWhat shall we do with a drunken sailor? \n"
+                 "What shall we do with a drunken sailor? \nEarly in the morning\n", "Way hay and up she rises \n"
+                 "Way hay and up she rises \nWay hay and up she rises \nEarly in the morning \n",
+                 'Find him, Bind him \nTie him to a pole and break \nHis fingers to splinters \n'
+                 'Drag him to a hole until he \nWakes up naked \nClawing at the ceiling \nOf his grave \n',
+                 'Whiskey drove me around Cape Horn \nWhiskey, Johnny! \nIt was many a month when I was gone \n'
+                 'Whiskey for my Johnny O!\n']
+
 filename1='cheer'
 filename2='curse'
 
